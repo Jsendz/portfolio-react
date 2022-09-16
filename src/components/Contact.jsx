cc84c12eaecfd22726ef3dc53f8be12e757a53f0
Jsendz <joshuasendzul@gmail.com>
2022-09-16 19:11:53 +0200

    react portfolio1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <div name='contact' className="w-full h-screen bg-[#012F44] flex justify-center items-center p-4 ">
         <form action="" className="flex flex-col max-w-[600px w-full]">
             <div className="pb-8">
-                <p className="text-4xl font-bold inline border-b-4 border-[#EA604D] text-gray-300">Contact</p>
-                <p className=" text-gray-300 py-4">Submit the form below or shoot me an email</p>
+                <p className="text-4xl font-bold inline border-b-4 border-[#EA604D] text-gray-300">{t("Contact.h-contact")}</p>
+                <p className=" text-gray-300 py-4">{t("Contact.p-contact")}</p>
             </div>
             <input className="bg-[#ccd6f6] p-2" type="text" placeholder='Name' name='name' />
             <input className="my-4 p-2 bg-[#ccd6f6] " type="email" placeholder='Email' name='email' />
