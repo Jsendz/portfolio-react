f8b91c91565010ba591f953ea042094754fc0a61
Jsendz <joshuasendzul@gmail.com>
2022-09-16 19:13:59 +0200

    react portfolio1

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,7 +26,7 @@ const Work = () => {
 
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="https://www.thesweethomecompany.com/">
+                            <a href="https://serene-einstein-e4215d.netlify.app/">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                             </a>
                             <a href="/">
@@ -44,7 +44,7 @@ const Work = () => {
 
                         </span>
                         <div className="pt-8 text-center">
-                            <a href="/">
+                            <a href="https://www.thesweethomecompany.com/">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
                             </a>
                             <a href="/">
